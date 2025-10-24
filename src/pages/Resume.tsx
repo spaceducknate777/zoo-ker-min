20642dfcbddab63aa8ed66174ef625196798a882
spaceducknate777 <natezukerman@gmail.com>
2025-10-24 19:15:16 -0400

    Refactor Resume component to use VisualResume

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,13 +1,7 @@
-import React from "react";
+import VisualResume from "@/resume/VisualResume";
 
-export default function Resume() {
-  return (
-    <div style={{ width: "100%", height: "100vh" }}>
-      <iframe
-        src="https://resume-ux-glow-up-vseh.vercel.app/"
-        style={{ width: "100%", height: "100%", border: "none" }}
-        title="Interactive Resume"
-      ></iframe>
-    </div>
-  );
-}
+const ResumePage = () => {
+  return <VisualResume />;
+};
+
+export default ResumePage;
