aca56b3f36652d5c455c5c58f1b5d53f09c5f197
spaceducknate777 <natezukerman@gmail.com>
2025-10-24 23:11:50 -0400

    Refactor main.tsx to use React.StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css"; // optional, if you use Tailwind or other CSS
 
-createRoot(document.getElementById("root")!).render(<App />);
-
-import "./resume/index.css";
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
